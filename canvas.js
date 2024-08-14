8a9923eeff9b5a1b2de5eb00e2692393193172c3
job moore <jobmurdan@ohtmail.com>
2024-08-13 19:57:56 -0700

    primer push

---
+++ canvas.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
+
+// ctx.fillStyle = "gray";
+// ctx.fillRect(0, 0, 500, 500);
+ctx.fillStyle = "rgb(248, 149, 0)";
+ctx.fillRect(10, 10, 150, 100);
+ctx.fillStyle = "rgb(117, 23, 205)";
+ctx.fillRect(40, 40, 150, 100);
+ctx.fillStyle = "rgb(233, 8, 173)";
+ctx.fillRect(25, 25, 150, 100);
