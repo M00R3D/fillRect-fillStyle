b1a60949b52fa1965dbf6bfc609f3324b0b4c016
job moore <jobmurdan@ohtmail.com>
2024-08-26 17:01:34 -0700

    18.-Con ayuda de los eventos keydown realizar la siguiente figura

--- canvas.js
+++ canvas.js
@@ -1,6 +1,35 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+fillStyle = "rgba(100, 149, 237, 0.5)";
+const player = 
+{
+    x: 50,      y: 50,      width: 50,      height: 50,     color: 'red'
+};
+ctx.fillStyle = player.color;
+ctx.fillRect(player.x, player.y, player.width, player.height);
 
+
+document.addEventListener("keypress", function(event) 
+{
+    console.log("Tecla presionada: " + event.key);
+    
+    if(event.key=="a" || event.key=="A"){player.x=player.x-2}
+    if(event.key=="d" || event.key=="D"){player.x=player.x+2}
+    if(event.key=="w" || event.key=="W"){player.y=player.y-2}
+    if(event.key=="s" || event.key=="S"){player.y=player.y+2}
+
+    ctx.fillStyle = "rgba(100, 149, 237, 0.5)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = getRandomColor();
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+});
+function getRandomColor() 
+{
+    var letters = '0123456789ABCDEF'.split('');         var color = '#';
+    for (var i = 0; i < 6; i++ ) {color += letters[Math.round(Math.random() * 15)];}
+    return color;
+}
 
 
 // ctx.fillStyle = "gray";
@@ -11,46 +40,33 @@ const ctx = canvas.getContext("2d");
 // ctx.fillRect(40, 40, 150, 100);
 // ctx.fillStyle = "rgb(248, 149, 0)";
 // ctx.fillRect(25, 25, 150, 100);
-
 // ctx.beginPath();
 // ctx.arc(200, 75, 50, 0, 2 * Math.PI);
 // ctx.stroke();
 // ctx.beginPath();
 // ctx.arc(300, 75, 50, 0, 2 * Math.PI);
 // ctx.fill();
-
 // ctx.beginPath();
 // ctx.arc(200, 75, 30, 0, Math.PI, false); 
 // ctx.fillStyle = "rgb(248, 49, 110)"; 
 // ctx.fill();
 // ctx.stroke();
-
 // ctx.font = "30px Arial";
-
 // ctx.fillStyle = "rgb(0, 0, 0)"; 
 // ctx.fillText("Texto", 50, 170); 
 // ctx.strokeStyle = "rgb(255, 220, 125)"; 
 // ctx.strokeText("Texto", 50, 170);
-ctx.fillStyle = "rgba(100, 149, 237, 0.5)";
-canvas.addEventListener("click", function(event) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;const y = event.clientY - rect.top;
+// canvas.addEventListener("click", function(event) {
+//     const rect = canvas.getBoundingClientRect();
+//     const x = event.clientX - rect.left;const y = event.clientY - rect.top;
 
-    ctx.beginPath();
-    ctx.arc(x, y, 50, 0, 2 * Math.PI); 
-    ctx.fill();
-    ctx.lineWidth = 2; 
-    ctx.strokeStyle = "blue";
-    ctx.stroke();
-});
-
-canvas.addEventListener("mouseout", function(event) {
-    ctx.fillStyle = getRandomColor();
-});
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');         var color = '#';
-
-    for (var i = 0; i < 6; i++ ) {color += letters[Math.round(Math.random() * 15)];}
-    return color;
-}
+//     ctx.beginPath();
+//     ctx.arc(x, y, 50, 0, 2 * Math.PI); 
+//     ctx.fill();
+//     ctx.lineWidth = 2; 
+//     ctx.strokeStyle = "blue";
+//     ctx.stroke();
+// });
+// canvas.addEventListener("mouseover", function(event) {
+//     ctx.fillStyle = getRandomColor();
+// });
